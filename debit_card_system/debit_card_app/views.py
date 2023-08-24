8fec81f8e9b60943556e89dfda600073f92bfd8b
Alejandro Chavez Gomez <alex.cg@ibm.com>
2023-08-24 16:44:34 -0600

    complete cuenta methods

--- debit_card_system/debit_card_app/views.py
+++ debit_card_system/debit_card_app/views.py
@@ -27,3 +27,34 @@ def eliminar_usuario(request, usuario_id):
         usuario.delete()
         return redirect('listar_usuarios')
     return render(request, 'eliminar_usuario.html', {'usuario': usuario})  # Create a corresponding template
+
+# Views and controllers for Cuenta
+
+def crear_cuenta(request, usuario_id):
+    usuario = get_object_or_404(Usuario, id=usuario_id)
+    if request.method == 'POST':
+        saldo = request.POST['saldo']
+        nueva_cuenta = Cuenta(usuario=usuario, saldo=saldo)
+        nueva_cuenta.save()
+        return redirect('leer_usuario', usuario_id=usuario.id)
+    return render(request, 'crear_cuenta.html', {'usuario': usuario})
+
+def leer_cuenta(request, cuenta_id):
+    cuenta = get_object_or_404(Cuenta, id=cuenta_id)
+    return render(request, 'leer_cuenta.html', {'cuenta': cuenta})
+
+def actualizar_cuenta(request, cuenta_id):
+    cuenta = get_object_or_404(Cuenta, id=cuenta_id)
+    if request.method == 'POST':
+        cuenta.saldo = request.POST['saldo']
+        cuenta.save()
+        return redirect('leer_cuenta', cuenta_id=cuenta.id)
+    return render(request, 'actualizar_cuenta.html', {'cuenta': cuenta})
+
+def eliminar_cuenta(request, cuenta_id):
+    cuenta = get_object_or_404(Cuenta, id=cuenta_id)
+    usuario_id = cuenta.usuario.id
+    if request.method == 'POST':
+        cuenta.delete()
+        return redirect('leer_usuario', usuario_id=usuario_id)
+    return render(request, 'eliminar_cuenta.html', {'cuenta': cuenta})
--- debit_card_system/debit_card_system/urls.py
+++ debit_card_system/debit_card_system/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     path('usuarios/<int:usuario_id>/editar/', views.actualizar_usuario, name='actualizar_usuario'),
     path('usuarios/<int:usuario_id>/eliminar/', views.eliminar_usuario, name='eliminar_usuario'),
 
+        # URLs for Cuenta
+    path('usuarios/<int:usuario_id>/cuentas/', views.crear_cuenta, name='crear_cuenta'),
+    path('usuarios/<int:usuario_id>/cuentas/<int:cuenta_id>/', views.leer_cuenta, name='leer_cuenta'),
+    path('usuarios/<int:usuario_id>/cuentas/<int:cuenta_id>/editar/', views.actualizar_cuenta, name='actualizar_cuenta'),
+    path('usuarios/<int:usuario_id>/cuentas/<int:cuenta_id>/eliminar/', views.eliminar_cuenta, name='eliminar_cuenta'),
+
 ]
