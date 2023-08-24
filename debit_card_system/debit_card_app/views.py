128fb173aa602276de14853780179d3e524bd93f
Jose Ramon Diaz Lara <ramon.diaz@ibm.com>
2023-08-24 16:50:10 -0600

    crear vistas y metodos de tarjetas

--- debit_card_system/debit_card_app/views.py
+++ debit_card_system/debit_card_app/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Usuario, Cuenta, Tarjeta
 
+
 def crear_usuario(request):
     if request.method == 'POST':
         nombre = request.POST['nombre']
         nuevo_usuario = Usuario(nombre=nombre)
         nuevo_usuario.save()
         return redirect('leer_usuario', usuario_id=nuevo_usuario.id)
-    return render(request, 'crear_usuario.html')  # Create a corresponding template
+    # Create a corresponding template
+    return render(request, 'crear_usuario.html')
+
 
 def leer_usuario(request, usuario_id):
     usuario = get_object_or_404(Usuario, id=usuario_id)
-    return render(request, 'leer_usuario.html', {'usuario': usuario})  # Create a corresponding template
+    # Create a corresponding template
+    return render(request, 'leer_usuario.html', {'usuario': usuario})
+
 
 def actualizar_usuario(request, usuario_id):
     usuario = get_object_or_404(Usuario, id=usuario_id)
@@ -19,16 +24,20 @@ def actualizar_usuario(request, usuario_id):
         usuario.nombre = request.POST['nombre']
         usuario.save()
         return redirect('leer_usuario', usuario_id=usuario.id)
-    return render(request, 'actualizar_usuario.html', {'usuario': usuario})  # Create a corresponding template
+    # Create a corresponding template
+    return render(request, 'actualizar_usuario.html', {'usuario': usuario})
+
 
 def eliminar_usuario(request, usuario_id):
     usuario = get_object_or_404(Usuario, id=usuario_id)
     if request.method == 'POST':
         usuario.delete()
         return redirect('listar_usuarios')
-    return render(request, 'eliminar_usuario.html', {'usuario': usuario})  # Create a corresponding template
+    # Create a corresponding template
+    return render(request, 'eliminar_usuario.html', {'usuario': usuario})
 
 # Views and controllers for Cuenta
+
 
 def crear_cuenta(request, usuario_id):
     usuario = get_object_or_404(Usuario, id=usuario_id)
@@ -39,9 +48,11 @@ def crear_cuenta(request, usuario_id):
         return redirect('leer_usuario', usuario_id=usuario.id)
     return render(request, 'crear_cuenta.html', {'usuario': usuario})
 
+
 def leer_cuenta(request, cuenta_id):
     cuenta = get_object_or_404(Cuenta, id=cuenta_id)
     return render(request, 'leer_cuenta.html', {'cuenta': cuenta})
+
 
 def actualizar_cuenta(request, cuenta_id):
     cuenta = get_object_or_404(Cuenta, id=cuenta_id)
@@ -51,6 +62,7 @@ def actualizar_cuenta(request, cuenta_id):
         return redirect('leer_cuenta', cuenta_id=cuenta.id)
     return render(request, 'actualizar_cuenta.html', {'cuenta': cuenta})
 
+
 def eliminar_cuenta(request, cuenta_id):
     cuenta = get_object_or_404(Cuenta, id=cuenta_id)
     usuario_id = cuenta.usuario.id
@@ -58,3 +70,38 @@ def eliminar_cuenta(request, cuenta_id):
         cuenta.delete()
         return redirect('leer_usuario', usuario_id=usuario_id)
     return render(request, 'eliminar_cuenta.html', {'cuenta': cuenta})
+
+# Views and controllers for Tarjeta
+
+
+def crear_tarjeta(request, cuenta_id):
+    cuenta = get_object_or_404(Cuenta, id=cuenta_id)
+    if request.method == 'POST':
+        balance = request.POST['balance']
+        nueva_tarjeta = Tarjeta(cuenta=cuenta, balance=balance)
+        nueva_tarjeta.save()
+        return redirect('leer_cuenta', cuenta_id=cuenta.id)
+    return render(request, 'crear_tarjeta.html', {'cuenta': cuenta})
+
+
+def leer_tarjeta(request, tarjeta_id):
+    tarjeta = get_object_or_404(Tarjeta, id=tarjeta_id)
+    return render(request, 'leer_tarjeta.html', {'tarjeta': tarjeta})
+
+
+def actualizar_tarjeta(request, tarjeta_id):
+    tarjeta = get_object_or_404(Tarjeta, id=tarjeta_id)
+    if request.method == 'POST':
+        tarjeta.balance = request.POST['balance']
+        tarjeta.save()
+        return redirect('leer_tarjeta', tarjeta_id=tarjeta.id)
+    return render(request, 'actualizar_tarjeta.html', {'tarjeta': tarjeta})
+
+
+def eliminar_tarjeta(request, tarjeta_id):
+    tarjeta = get_object_or_404(Tarjeta, id=tarjeta_id)
+    cuenta_id = tarjeta.cuenta.id
+    if request.method == 'POST':
+        tarjeta.delete()
+        return redirect('leer_cuenta', cuenta_id=cuenta_id)
+    return render(request, 'eliminar_tarjeta.html', {'tarjeta': tarjeta})
--- debit_card_system/debit_card_system/urls.py
+++ debit_card_system/debit_card_system/urls.py
@@ -1,19 +1,35 @@
-#Urls
+# Urls
 from django.urls import path
-from debit_card_app import views 
+from debit_card_app import views
 
 urlpatterns = [
-    
+
     # URLs for Usuario
     path('usuarios/crear/', views.crear_usuario, name='crear_usuario'),
     path('usuarios/<int:usuario_id>/', views.leer_usuario, name='leer_usuario'),
-    path('usuarios/<int:usuario_id>/editar/', views.actualizar_usuario, name='actualizar_usuario'),
-    path('usuarios/<int:usuario_id>/eliminar/', views.eliminar_usuario, name='eliminar_usuario'),
+    path('usuarios/<int:usuario_id>/editar/',
+         views.actualizar_usuario, name='actualizar_usuario'),
+    path('usuarios/<int:usuario_id>/eliminar/',
+         views.eliminar_usuario, name='eliminar_usuario'),
 
-        # URLs for Cuenta
-    path('usuarios/<int:usuario_id>/cuentas/', views.crear_cuenta, name='crear_cuenta'),
-    path('usuarios/<int:usuario_id>/cuentas/<int:cuenta_id>/', views.leer_cuenta, name='leer_cuenta'),
-    path('usuarios/<int:usuario_id>/cuentas/<int:cuenta_id>/editar/', views.actualizar_cuenta, name='actualizar_cuenta'),
-    path('usuarios/<int:usuario_id>/cuentas/<int:cuenta_id>/eliminar/', views.eliminar_cuenta, name='eliminar_cuenta'),
+    # URLs for Cuenta
+    path('usuarios/<int:usuario_id>/cuentas/',
+         views.crear_cuenta, name='crear_cuenta'),
+    path('usuarios/<int:usuario_id>/cuentas/<int:cuenta_id>/',
+         views.leer_cuenta, name='leer_cuenta'),
+    path('usuarios/<int:usuario_id>/cuentas/<int:cuenta_id>/editar/',
+         views.actualizar_cuenta, name='actualizar_cuenta'),
+    path('usuarios/<int:usuario_id>/cuentas/<int:cuenta_id>/eliminar/',
+         views.eliminar_cuenta, name='eliminar_cuenta'),
+
+    # URLs for Tarjeta
+    path('usuarios/<int:usuario_id>/cuentas/<int:cuenta_id>/tarjetas/',
+         views.crear_tarjeta, name='crear_tarjeta'),
+    path('usuarios/<int:usuario_id>/cuentas/<int:cuenta_id>/tarjetas/<int:tarjeta_id>/',
+         views.leer_tarjeta, name='leer_tarjeta'),
+    path('usuarios/<int:usuario_id>/cuentas/<int:cuenta_id>/tarjetas/<int:tarjeta_id>/editar/',
+         views.actualizar_tarjeta, name='actualizar_tarjeta'),
+    path('usuarios/<int:usuario_id>/cuentas/<int:cuenta_id>/tarjetas/<int:tarjeta_id>/eliminar/',
+         views.eliminar_tarjeta, name='eliminar_tarjeta'),
 
 ]
